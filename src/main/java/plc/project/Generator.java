ca5552c0d4b38077a8ba389ca7aeba664d4bb7ce
erobx <erob7856@gmail.com>
2024-04-08 21:59:59 +0200

    Literal, group, binary expressions
    Signed-off-by: erobx <erob7856@gmail.com>

---
+++ src/main/java/plc/project/Generator.java
@@ -0,0 +1,147 @@
+package plc.project;
+
+import java.io.PrintWriter;
+
+public final class Generator implements Ast.Visitor<Void> {
+
+    private final PrintWriter writer;
+    private int indent = 0;
+
+    public Generator(PrintWriter writer) {
+        this.writer = writer;
+    }
+
+    private void print(Object... objects) {
+        for (Object object : objects) {
+            if (object instanceof Ast) {
+                visit((Ast) object);
+            } else {
+                writer.write(object.toString());
+            }
+        }
+    }
+
+    private void newline(int indent) {
+        writer.println();
+        for (int i = 0; i < indent; i++) {
+            writer.write("    ");
+        }
+    }
+
+    @Override
+    public Void visit(Ast.Source ast) {
+
+
+        return null;
+    }
+
+    @Override
+    public Void visit(Ast.Global ast) {
+        throw new UnsupportedOperationException(); //TODO
+    }
+
+    @Override
+    public Void visit(Ast.Function ast) {
+        throw new UnsupportedOperationException(); //TODO
+    }
+
+    @Override
+    public Void visit(Ast.Statement.Expression ast) {
+        throw new UnsupportedOperationException(); //TODO
+    }
+
+    @Override
+    public Void visit(Ast.Statement.Declaration ast) {
+        throw new UnsupportedOperationException(); //TODO
+    }
+
+    @Override
+    public Void visit(Ast.Statement.Assignment ast) {
+        throw new UnsupportedOperationException(); //TODO
+    }
+
+    @Override
+    public Void visit(Ast.Statement.If ast) {
+        throw new UnsupportedOperationException(); //TODO
+    }
+
+    @Override
+    public Void visit(Ast.Statement.Switch ast) {
+        throw new UnsupportedOperationException(); //TODO
+    }
+
+    @Override
+    public Void visit(Ast.Statement.Case ast) {
+        throw new UnsupportedOperationException(); //TODO
+    }
+
+    @Override
+    public Void visit(Ast.Statement.While ast) {
+        throw new UnsupportedOperationException(); //TODO
+    }
+
+    @Override
+    public Void visit(Ast.Statement.Return ast) {
+        throw new UnsupportedOperationException(); //TODO
+    }
+
+    @Override
+    public Void visit(Ast.Expression.Literal ast) {
+        // For strings and characters have to print literal ' & "
+        if (ast.getLiteral() instanceof String) {
+            print("\"");
+            print(ast.getLiteral());
+            print("\"");
+        } else if (ast.getLiteral() instanceof Character) {
+            print("'");
+            print(ast.getLiteral());
+            print("'");
+        } else if (ast.getLiteral() != null) {
+            print(ast.getLiteral());
+        } else {
+            print("null");
+        }
+        return null;
+    }
+
+    @Override
+    public Void visit(Ast.Expression.Group ast) {
+        print("(");
+        visit(ast.getExpression());
+        print(")");
+        return null;
+    }
+
+    @Override
+    public Void visit(Ast.Expression.Binary ast) {
+        if (ast.getOperator().equals("^")) {
+            // Math.pow(left, right)
+            print("Math.pow(");
+            visit(ast.getLeft());
+            print(", ");
+            visit(ast.getRight());
+            print(")");
+            return null;
+        }
+        visit(ast.getLeft());
+        print(" " + ast.getOperator() + " ");
+        visit(ast.getRight());
+        return null;
+    }
+
+    @Override
+    public Void visit(Ast.Expression.Access ast) {
+        throw new UnsupportedOperationException(); //TODO
+    }
+
+    @Override
+    public Void visit(Ast.Expression.Function ast) {
+        throw new UnsupportedOperationException(); //TODO
+    }
+
+    @Override
+    public Void visit(Ast.Expression.PlcList ast) {
+        throw new UnsupportedOperationException(); //TODO
+    }
+
+}
